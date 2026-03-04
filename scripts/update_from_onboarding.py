b693bbdb3a9803f338bc79e17fc196d070f69860
SOUMIK ROY <soumikroy7272@gmail.com>
2026-03-04 15:54:34 +0530

    Updated the onboarding

---
+++ scripts/update_from_onboarding.py
@@ -0,0 +1,14 @@
+import json
+import os
+from datetime import datetime
+from scripts.generate_prompt import build_agent_spec
+
+
+def load_json(path):
+    with open(path) as f:
+        return json.load(f)
+
+
+def save_json(path, data):
+    with open(path, "w") as f:
+        json.dump(data, f, indent=2)
